0257f93f6339b033d934a09610da6fed7ce78182
Hmily <961532186@qq.com>
2024-09-26 17:08:49 +0800

    🐛 Fix log print for utils

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -51,7 +51,7 @@ def send_email(
     if settings.SMTP_PASSWORD:
         smtp_options["password"] = settings.SMTP_PASSWORD
     response = message.send(to=email_to, smtp=smtp_options)
-    logging.info(f"send email result: {response}")
+    logger.info(f"send email result: {response}")
 
 
 def generate_test_email(email_to: str) -> EmailData:
